acadaaf53882dcaa174ab6ceb99fc29207eb9013
Artem Elizarkov <elizarkov.av@gmail.com>
2024-10-28 14:48:53 +0300

    added pageSource and Logs

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -19,6 +19,8 @@ public class TestBase {
     @AfterEach
     void afterEach() {
         Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
         Selenide.closeWebDriver();
     }
 }
