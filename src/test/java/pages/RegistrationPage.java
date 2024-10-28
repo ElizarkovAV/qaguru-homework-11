596e49001278f18fd26ed6163d1a46f386df48cf
Artem Elizarkov <elizarkov.av@gmail.com>
2024-10-28 13:48:33 +0300

    added steps

--- src/test/java/pages/RegistrationPage.java
+++ src/test/java/pages/RegistrationPage.java
@@ -1,6 +1,7 @@
 package pages;
 
 import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
 import pages.components.CalendarComponent;
 import pages.components.ResultsTable;
 
@@ -30,38 +31,52 @@ public class RegistrationPage {
 
     CalendarComponent calendarComponent = new CalendarComponent();
 
+    @Step("Open registration page")
     public RegistrationPage openPage() {
         open("/automation-practice-form");
         executeJavaScript("$('#fixedban').remove()");
         executeJavaScript("$('footer').remove()");
         return this;
     }
+
+    @Step("Set first name")
     public RegistrationPage setFirstName (String value) {
         firstNameInput.setValue(value);
         return this;
     }
+
+    @Step("Set last name")
     public RegistrationPage setLastName (String value) {
         lastNameInput.setValue(value);
         return this;
     }
 
+    @Step("Set email")
     public RegistrationPage setEmail(String value) {
         emailInput.setValue(value);
         return this;
     }
+
+    @Step("Set gender")
     public RegistrationPage setGender (String value) {
         genderWrapper.$(byText(value)).click();
         return this;
     }
+
+    @Step("set user number")
     public RegistrationPage setUserNumber(String value) {
         userNumberInput.setValue(value);
         return this;
     }
+
+    @Step("set date of birth")
     public RegistrationPage setDateOfBirth (String day, String month, String year) {
         calendarInput.click();
         calendarComponent.setDate(day, month, year);
         return this;
     }
+
+    @Step("Check results")
     public RegistrationPage checkResult(String key, String value) {
         new ResultsTable().checkResult(key, value);
         return this;
@@ -74,11 +89,13 @@ public class RegistrationPage {
         return this;
     }
 
+    @Step("Set subject")
     public RegistrationPage setSubject(String value) {
         subjectInput.setValue(value).pressEnter();
         return this;
     }
 
+    @Step("Set subjects")
     public RegistrationPage setSubjects(List<String> values) {
         for (String value : values) {
             subjectInput.setValue(value).pressEnter();
@@ -86,21 +103,25 @@ public class RegistrationPage {
         return this;
     }
 
+    @Step("Set hobby")
     public RegistrationPage setHobby(String hobby) {
         hobbyRadioBtn.$(byText(hobby)).click();
         return this;
     }
 
+    @Step("Upload image")
     public RegistrationPage uploadImage(String value) {
         uploadImageInput.uploadFromClasspath("images/" + value);
         return this;
     }
 
+    @Step("Set address")
     public RegistrationPage setAddress(String value) {
         addressInput.setValue(value);
         return this;
     }
 
+    @Step("Set state")
     public RegistrationPage setState (String value) {
         stateTabs.scrollTo();
         stateTabs.click();
@@ -108,6 +129,7 @@ public class RegistrationPage {
         return this;
     }
 
+    @Step("Set city")
     public RegistrationPage setCity (String value) {
         cityTabs.scrollTo();
         cityTabs.click();
@@ -115,6 +137,7 @@ public class RegistrationPage {
         return this;
     }
 
+    @Step("Click to submit")
     public RegistrationPage submit() {
         submitBtn.click();
         return this;
