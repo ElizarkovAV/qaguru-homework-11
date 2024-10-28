14c102a39bf56a174dd27f671a076211af3ad889
Artem Elizarkov <elizarkov.av@gmail.com>
2024-10-28 15:13:15 +0300

    add capabilities

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -21,10 +21,13 @@ public class TestBase {
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
+
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
                 "enableVNC", true,
                 "enableVideo", true
         ));
+
+        Configuration.browserCapabilities = capabilities;
     }
 
     @AfterEach
